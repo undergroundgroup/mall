442d2d83fbf5f44feb06c9cd107e8a48bd351a8a
xyb <1156295142@qq.com>
2019-05-09 22:17:34 +0800

    1

---
+++ mall/src/main/java/com/market/mall/web/BizException.java
@@ -0,0 +1,25 @@
+package com.market.mall.web;
+
+public class BizException extends Exception {
+
+	public BizException() {
+		super();
+	}
+
+	public BizException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public BizException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BizException(String message) {
+		super(message);
+	}
+
+	public BizException(Throwable cause) {
+		super(cause);
+	}
+	
+}
